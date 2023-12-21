ad962201c599fbac02685ee133a99f21e98c5252
Oryan Abergel <86202122+Oryanab@users.noreply.github.com>
2023-12-21 22:42:06 +0200

    update

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,16 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { ReservationsModule } from './reservations.module';
+import { AuthModule } from './auth.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ReservationsModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true, // The validtor will ignore every param which is not part of DTO
-    }),
-  );
+  const app = await NestFactory.create(AuthModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
